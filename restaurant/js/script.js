0aec390bfb10a1c3e6c038f26d8d3871060270bf
Mohamamd-Haikal <mohammad.haikal@htu..edu.jo>
2021-09-09 23:58:14 +0300

    .

--- restaurant/js/script.js
+++ restaurant/js/script.js
@@ -8,30 +8,43 @@ $(function () {
         dataType: "json",
         success: function (data) {
             let propertyNames = Object.getOwnPropertyNames(data);
-            console.log(data[propertyNames[1]].length);
-            
-            for (let i = 0; i < propertyNames.length; i++) {
+            console.log(data[1].info.length);
+
+            for (let i = 0; i < data.length; i++) {
+                console.log(data[i]);
                 $('#menuDiv').append(
                     `
-                    <section id="${propertyNames[i]}">
-                    <h1>${data[propertyNames[i]][0].name}</h1>
+                    <section id="${data[i].id}">
+                    <h1>${data[i].name}</h1>
 
-                    </section>           
+                    </section>            
                     `
                 );
-            };
+                
+            }
+            
+            // for (let i = 0; i < propertyNames.length; i++) {
+            //     $('#menuDiv').append(
+            //         `
+            //         <section id="${propertyNames[i]}">
+            //         <h1>${data[propertyNames[i]][0].name}</h1>
 
-            for (let j = 0; j < propertyNames.length; j++) {
-                for (let k = 0; k < data[propertyNames[j]].length; k++) {
+            //         </section>           
+            //         `
+            //     );
+            // };
 
-                    $(`#${propertyNames[j]}`).append(
+            for (let i = 0; i < data.length; i++) {
+                for (let k = 0; k < data[i].info.length; k++) {
+
+                    $(`#${data[i].id}`).append(
                         `
                         <div class="food-type">
                             <div class="description">
-                                <h4>${data[propertyNames[j]][k].title}</h4>
-                                <p>${data[propertyNames[j]][k].title}</p>
+                                <h4>${data[i].info[k].title}</h4>
+                                <p>${data[i].info[k].para}</p>
                             </div>
-                            <img src="${data[propertyNames[j]][k].path}" alt="${propertyNames[0]}"> 
+                            <img src="${data[i].info[k].path}" alt="${data[i].info[k].title}"> 
                         </div>
                         `
                     );
