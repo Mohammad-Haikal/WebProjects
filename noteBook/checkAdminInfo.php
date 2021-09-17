62138b551ed352cc7325dd8ddfa92a57b1156e0e
Mohamamd-Haikal <mohammad.haikal@htu..edu.jo>
2021-09-18 01:50:19 +0300

    .

--- noteBook/checkAdminInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-if($_POST["username"] == "muhnouf" && $_POST["password"] == "muhnouf"){
-    session_start();
-    $_SESSION['loggedin'] = TRUE;
-    header("Location: index2.php");
-}else{
-    header("Location: index.php");
-}
-?>
