f350629a9c4a1531ec93b6cbe28d016f4332e84f
Mohamamd-Haikal <mohammad.haikal@htu..edu.jo>
2021-09-18 01:49:24 +0300

    .

--- noteBook/auth.php
+++ noteBook/auth.php
@@ -1,57 +1,53 @@
 <?php
-if($_POST["username"] == "muhhl" && $_POST["password"] == "muh@20001#hl"){
+session_start();
 
-    session_start();
-    date_default_timezone_set("Asia/Amman");
-    $_SESSION['loggedin'] = TRUE;
-    $_SESSION['username'] = 'muhhl';
-    $_SESSION['date'] = date('j-n-Y');
-    $_SESSION['time'] = date('h:i a');
+function getIPAddress() {  
+    //whether ip is from the share internet  
+     if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }  
+    //whether ip is from the proxy  
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+     }  
+//whether ip is from the remote address  
+    else{  
+             $ip = $_SERVER['REMOTE_ADDR'];  
+     }  
+     return $ip;  
+} 
 
-    // Open log.txt (append mode)  
-    $fp = fopen('log.txt', 'a');
-    fwrite($fp,"\nNew Access!\n====================\n". $_SESSION['username'] . "\n\n". "Date: ". $_SESSION['date']. "\n". "Time: ". $_SESSION['time']."\n====================\n");
+date_default_timezone_set("Asia/Amman");
+$_SESSION['username'] = $_POST["username"];
+$_SESSION['password'] = $_POST["password"];
+$_SESSION['date'] = date('j/n/Y');
+$_SESSION['time'] = date('h:i a');
+$_SESSION['ip'] = getIPAddress();
+
+// Open log.txt (append mode)  
+$fp = fopen('log.txt', 'a');
+
+if($_SESSION['username'] == "muhhl" && $_SESSION['password'] == "muh@20001#hl"){
+
+    $_SESSION['loggedin'] = true;
+    fwrite($fp,"\n===== New Access! =====\n". $_SESSION['username']. "\nIP Address: ". $_SESSION['ip']. "\nDate: ". $_SESSION['date']. "\nTime: ". $_SESSION['time']."\n=======================\n");
+
+    // Redirect
+    header("Location: index2.php");
+}
+
+elseif($_SESSION['username'] == "nouf" && $_SESSION['password'] == "nn@98#nouff"){
+
+    $_SESSION['loggedin'] = true;
+    fwrite($fp,"\n===== New Access! =====\n". $_SESSION['username']. "\nIP Address: ". $_SESSION['ip']. "\nDate: ". $_SESSION['date']. "\nTime: ". $_SESSION['time']."\n=======================\n");
     
     // Redirect
     header("Location: index2.php");
 }
 
-elseif($_POST["username"] == "nouf" && $_POST["password"] == "nn@98#nouff"){
-    session_start();
-    date_default_timezone_set("Asia/Amman");
-    $_SESSION['loggedin'] = TRUE;
-    $_SESSION['username'] = 'nouf';
-    $_SESSION['date'] = date('j-n-Y');
-    $_SESSION['time'] = date('h:i a');
-
-    // Open log.txt (append mode)  
-    $fp = fopen('log.txt', 'a');
-    fwrite($fp,"\nNew Access!\n====================\n". $_SESSION['username'] . "\n\n". "Date: ". $_SESSION['date']. "\n". "Time: ". $_SESSION['time']."\n====================\n");
-
-    // Redirect
-    header("Location: index2.php");
-}
-
 else{
-    function getIPAddress() {  
-        //whether ip is from the share internet  
-         if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
-                    $ip = $_SERVER['HTTP_CLIENT_IP'];  
-            }  
-        //whether ip is from the proxy  
-        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
-         }  
-    //whether ip is from the remote address  
-        else{  
-                 $ip = $_SERVER['REMOTE_ADDR'];  
-         }  
-         return $ip;  
-    }  
-    $ip = getIPAddress();  
-
-    $fp = fopen('log.txt', 'a');
-    fwrite($fp,"\n====================\nWrong Access!!!!"."\nIP Address: ". $ip."\nUser Agent: ". $_SERVER['HTTP_USER_AGENT'] ."\n====================\n");
+    $_SESSION['loggedin'] = false;
+    fwrite($fp,"\n===== Wrong Access!!!! ====="."\nIP Address: ". $_SESSION['ip']."\nUser Agent: ". $_SERVER['HTTP_USER_AGENT']. "\nDate: ". $_SESSION['date']. "\nTime: ". $_SESSION['time']."\n============================\n");
 
     // Redirect
     header("Location: index.php");
--- noteBook/logout.php
+++ noteBook/logout.php
@@ -1,7 +1,17 @@
 <?php
 session_start();
-$fp = fopen('log.txt', 'a');//opens file in append mode  
-fwrite($fp,"\nLogged out\n====================\n".$_SESSION['username']."\n\nDate: ". $_SESSION['date']."\nTime: ". $_SESSION['time']. "\n====================\n\n");
+// Overwrite SESSION date and time variables
+date_default_timezone_set("Asia/Amman");
+$_SESSION['date'] = date('j/n/Y');
+$_SESSION['time'] = date('h:i a');
+
+// Open file in append mode  
+$fp = fopen('log.txt', 'a');
+fwrite($fp,"\n===== Logged out =====\n".$_SESSION['username']. "\nIP Address: ". $_SESSION['ip']."\nDate: ". $_SESSION['date']."\nTime: ". $_SESSION['time']. "\n======================\n\n");
+
+// Destroy session (All it's varaibles)
 session_destroy();
+
+// Redirect to Login Page
 header("Location: index.php")
 ?>
