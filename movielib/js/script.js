6e70c5391de0446564a6e9ab8785e02cc575d50a
Mohamamd-Haikal <mohammad.haikal@htu..edu.jo>
2021-11-06 00:42:10 +0200

    .

--- movielib/js/script.js
+++ movielib/js/script.js
@@ -2,9 +2,9 @@ const movieDBKey = 'cb577b81e37a005b719395bb2cb58cdc';
 const userId = document.getElementById('userId').value;
 var movieName;
 var page = 1;
+const genres = [{ "id": 28, "name": "Action" }, { "id": 12, "name": "Adventure" }, { "id": 16, "name": "Animation" }, { "id": 35, "name": "Comedy" }, { "id": 80, "name": "Crime" }, { "id": 99, "name": "Documentary" }, { "id": 18, "name": "Drama" }, { "id": 10751, "name": "Family" }, { "id": 14, "name": "Fantasy" }, { "id": 36, "name": "History" }, { "id": 27, "name": "Horror" }, { "id": 10402, "name": "Music" }, { "id": 9648, "name": "Mystery" }, { "id": 10749, "name": "Romance" }, { "id": 878, "name": "Science Fiction" }, { "id": 10770, "name": "TV Movie" }, { "id": 53, "name": "Thriller" }, { "id": 10752, "name": "War" }, { "id": 37, "name": "Western" }]
 
 $(function () {
-
     $('.fav').click(function (e) {
         e.preventDefault();
         if ($(this).hasClass('fav-active')) {
@@ -39,19 +39,27 @@ $(function () {
                             movieImgPath = `../img/nopic.jpg`
                         }
 
-                        $('#container').append(`<article data-aos="zoom-in"><i id=${response[key].movieId} class="fa fa-minus favBtn favBtn-active"></i> <img src="${movieImgPath}"><h1>${response[key].title}</h1><h4>Released: ${response[key].release_date}</h4><p class="description">${response[key].overview}</p></article>`);
+                        $('#container').append(`<article data-aos="zoom-in"><i id=${response[key].movieId} class="fa fa-minus favBtn favBtn-active"></i> <img src="${movieImgPath}"><h1>${response[key].title}</h1><p class="genres${response[key].movieId}"></p><h4>Released: ${response[key].release_date}</h4><p class="description">${response[key].overview}</p></article>`);
+
+                        /////////////////////////////////////
+                        $(`.genres${response[key].movieId}`).html(response[key].genres.join(" | "));
+                        /////////////////////////////////////
+
+
+
                         $(`.favBtn#${response[key].movieId}`).click(function (e) {
 
                             let movieObject = {
                                 "movieId": response[key].movieId,
                                 "imgPath": response[key].imgPath,
                                 "title": response[key].title,
+                                "genres": response[key].genres,
                                 "release_date": response[key].release_date,
                                 "overview": response[key].overview
                             }
 
                             let ready = JSON.stringify(movieObject);
-                            
+
                             if ($(this).hasClass('favBtn-active')) {
                                 $(this).hide(100);
 
@@ -124,8 +132,21 @@ function showResuts(response) {
             movieImgPath = `../img/nopic.jpg`
         }
 
-        $('#container').append(`<article data-aos="zoom-in"><i id=${response.results[key].id} class="fa fa-plus favBtn"></i> <img src="${movieImgPath}"><h1>${response.results[key].title}</h1><h4>Released: ${response.results[key].release_date}</h4><p class="description">${response.results[key].overview}</p></article>`);
+        $('#container').append(`<article data-aos="zoom-in"><i id=${response.results[key].id} class="fa fa-plus favBtn"></i> <img src="${movieImgPath}"><h1>${response.results[key].title}</h1><p class="genres${response.results[key].id}"></p><h4>Released: ${response.results[key].release_date}</h4><p class="description">${response.results[key].overview}</p></article>`);
+        
+        /////////////////////////////////////
+        let movieGenres = [];
+        for (const j in response.results[key].genre_ids) {
+            genres.find(function (post, index) {
+                if (post.id == response.results[key].genre_ids[j]) {
+                    movieGenres.push(post.name);
+                }
+            });
+        }
 
+        $(`.genres${response.results[key].id}`).html(movieGenres.join(" | "));
+        /////////////////////////////////////
+        
         $(`.favBtn#${response.results[key].id}`).click(function (e) {
             e.preventDefault();
             $(this).hide(100);
@@ -134,6 +155,7 @@ function showResuts(response) {
                 'movieId': response.results[key].id,
                 'imgPath': response.results[key].backdrop_path,
                 'title': response.results[key].title,
+                'genres': movieGenres,
                 'release_date': response.results[key].release_date,
                 'overview': response.results[key].overview
             }
