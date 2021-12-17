9a0de48816a3cddb0c7d6f0f7d38b1bdc3013907
Mohamamd-Haikal <mohammad.haikal@htu..edu.jo>
2021-12-17 17:30:50 +0200

    .

---
+++ Student Project/js/script.js
@@ -0,0 +1,45 @@
+
+/////////////////////////////////////////////////////////////
+// Slider
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 3 seconds
+}
+
+
+/////////////////////////////////////////////////////////////
+// Animate the progress bars
+document.getElementById("html").value = 80;
+document.getElementById("css").value = 50;
+document.getElementById("js").value = 100;
+
+
+/////////////////////////////////////////////////////////////
+// Back To Top Button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
